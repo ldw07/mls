dc1875becd4206124c2d4acda0593ad052363c32
yjc <843401905@qq.com>
2020-05-16 03:35:51 +0800

    update

--- src/views/question.vue
+++ src/views/question.vue
@@ -51,7 +51,7 @@ import successBaseVue from './successBase.vue';
 console.error(data)
 
 const MAX_TIME = 10;//倒计时时间
-const QUES_AMOUNT = 4;//题目数量
+const QUES_AMOUNT = 6;//题目数量
 const MAX_FREQ = 3;//最大答题次数
 const ORDER = Math.floor(Math.random*10 - 1).toString;//0-9的随机数
 // const aaa = "base" + "1";
